d7f634e2fbc9e106857ecef3b1fbf5fe40a355f3
gdlyeabkov <gdlyeabkov@gmail.com>
2022-01-22 16:57:28 +0300

    add_dynamic_groups_output

--- Reposter/MainWindow.xaml.cs
+++ Reposter/MainWindow.xaml.cs
@@ -28,32 +28,65 @@ namespace Reposter
         public SpeechSynthesizer debugger;
         public string accessToken = "b2228cf06accc6c34160682dcd46e6232378bb04e996c85a5af76c14585a24b888266908af45d10de1fa8";
         public string vkUserId = "382177113";
+        public string vkGroupId = "-210273208";
+        public List<String> publishedGroups;
 
         public MainWindow()
         {
             InitializeComponent();
 
             debugger = new SpeechSynthesizer();
+            publishedGroups = new List<String>();
+            GetGroupsIds();
 
         }
 
         private void SendPostHandler(object sender, RoutedEventArgs e)
         {
             string myMessage = request.Text;
-            string uriPath = "https://api.vk.com/method/wall.post?owner_id=" + vkUserId + "&message=" + myMessage + "&access_token=" + accessToken + "&v=5.92";
+            foreach (string publishedGroupId in publishedGroups)
+            {
+                string uriPath = "https://api.vk.com/method/wall.post?owner_id=" + publishedGroupId + "&message=" + myMessage + "&access_token=" + accessToken + "&v=5.92";
+                var webRequest = HttpWebRequest.Create(uriPath);
+                webRequest.Method = "GET";
+                try
+                {
+                    using (var webResponse = webRequest.GetResponse())
+                    {
+                        using (var reader = new StreamReader(webResponse.GetResponseStream()))
+                        {
+                            JavaScriptSerializer js = new JavaScriptSerializer();
+                            var objText = reader.ReadToEnd();
+                            ResponseInfo myobj = (ResponseInfo)js.Deserialize(objText, typeof(ResponseInfo));
+                            debugger.Speak(myobj.post_id.ToString());
+                        }
+                    }
+                }
+                catch (WebException)
+                {
+                    debugger.Speak("Ошибка запроса");
+                }
+            }
+        }
+
+        public void GetGroupsIds()
+        {
+            string uriPath = "https://api.vk.com/method/groups.get?user_id=" + vkUserId + "&access_token=" + accessToken + "&v=5.92";
             var webRequest = HttpWebRequest.Create(uriPath);
             webRequest.Method = "GET";
             try
             {
                 using (var webResponse = webRequest.GetResponse())
                 {
-                    // debugger.Speak(webResponse.ContentLength.ToString());
                     using (var reader = new StreamReader(webResponse.GetResponseStream()))
                     {
                         JavaScriptSerializer js = new JavaScriptSerializer();
                         var objText = reader.ReadToEnd();
-                        ResponseInfo myobj = (ResponseInfo)js.Deserialize(objText, typeof(ResponseInfo));
-                        debugger.Speak(myobj.post_id.ToString());
+                        ResponseGroupsIdsInfo myobj = (ResponseGroupsIdsInfo)js.Deserialize(objText, typeof(ResponseGroupsIdsInfo));
+                        int[] groupsIds = myobj.response.items;
+                        string rawGroupsIds = String.Join(",", groupsIds);
+                        GetGroups(rawGroupsIds);
+                        debugger.Speak(myobj.response.items.Length.ToString());
                     }
                 }
             }
@@ -62,11 +95,107 @@ namespace Reposter
                 debugger.Speak("Ошибка запроса");
             }
         }
+
+        public void GetGroups(string rawGroupsIds)
+        {
+            string uriPath = "https://api.vk.com/method/groups.getById?group_ids=" + rawGroupsIds + "&access_token=" + accessToken + "&v=5.92";
+            var webRequest = HttpWebRequest.Create(uriPath);
+            webRequest.Method = "GET";
+            try
+            {
+                using (var webResponse = webRequest.GetResponse())
+                {
+                    using (var reader = new StreamReader(webResponse.GetResponseStream()))
+                    {
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        var objText = reader.ReadToEnd();
+                        ResponseGroupsInfo myobj = (ResponseGroupsInfo)js.Deserialize(objText, typeof(ResponseGroupsInfo));
+                        GroupInfo[] myGroups = myobj.response;
+                        foreach (GroupInfo group in myGroups)
+                        {
+                            StackPanel newGroup = new StackPanel();
+                            newGroup.Orientation = Orientation.Horizontal;
+                            newGroup.Height = 50;
+                            CheckBox newGroupCheckbox = new CheckBox();
+                            newGroupCheckbox.Margin = new Thickness(15);
+                            string parsedGroupId = "-" + group.id;
+                            newGroupCheckbox.DataContext = parsedGroupId;
+                            newGroupCheckbox.Click += ToggleGroupHandler;
+                            newGroup.Children.Add(newGroupCheckbox);
+                            TextBlock newGroupName = new TextBlock();
+                            newGroupName.Margin = new Thickness(15);
+                            newGroupName.Text = group.name;
+                            newGroup.Children.Add(newGroupName);
+                            groups.Children.Add(newGroup);
+                        }
+                        debugger.Speak(myobj.response.Length.ToString());
+                    }
+                }
+            }
+            catch (WebException)
+            {
+                debugger.Speak("Ошибка запроса");
+            }
+        }
+
+        private void ToggleGroupHandler (object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = ((CheckBox)(sender));
+            bool isGroupSelect = ((bool)(checkBox.IsChecked));
+            string currentGroupId = ((string)(checkBox.DataContext));
+            if (isGroupSelect)
+            {
+                debugger.Speak("Добавил группу " + currentGroupId);
+                publishedGroups.Add(currentGroupId);
+            }
+            else
+            {
+                debugger.Speak("Исключил группу " + currentGroupId);
+                int currentGroupIndex = publishedGroups.IndexOf(currentGroupId);
+                publishedGroups.RemoveAt(currentGroupIndex);
+            }
+            bool isGroupsUnselected = publishedGroups.Count <= 0;
+            if (isGroupsUnselected)
+            {
+                sendPostBtn.IsEnabled = false;
+            }
+        }
     }
 
     public class ResponseInfo
     {
         public int post_id;
+    }
+
+    public class ResponseGroupsIdsInfo
+    {
+        public GroupsIdsInfo response;
+    }
+
+    public class GroupsIdsInfo
+    {
+        public int count;
+        public int[] items;
+    }
+
+    public class ResponseGroupsInfo
+    {
+        public GroupInfo[] response;
+    }
+
+    public class GroupInfo
+    {
+        public int id;
+        public string name;
+        public string screen_name;
+        public int is_closed;
+        public string type;
+        public int is_admin;
+        public int is_member;
+        public int is_advertiser;
+        public string photo_50;
+        public string photo_100;
+        public string photo_200;
     }
 
 }
